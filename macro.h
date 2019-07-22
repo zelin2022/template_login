968a6b97729f2d0579eba197d57e21995a1cbbb8
Zelin Liu <zelinliu@hotmail.com>
2019-07-22 00:03:27 -0600

    not much

---
+++ macro.h
@@ -0,0 +1,7 @@
+/*
+* Filename: macro.h
+* Author: Zelin Liu
+* Date: 2019/07/21
+* Description: header file for macros for template login project
+* Instruction: -
+*/
---
+++ template_login.cpp
@@ -0,0 +1,11 @@
+/*
+* Filename: template_login.cpp
+* Author: Zelin Liu
+* Date: 2019/07/21
+* Description: main file for template login project
+* Instruction: compile with g++
+*/
+
+int main(int argc, char** argv){
+
+}
